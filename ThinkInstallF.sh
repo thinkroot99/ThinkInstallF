f23a6e5751fd84d32bcb99da5a17f4cc22b55752
ThinkRoot99 <social@crism.ro>
2024-03-13 11:50:20 +0200

    Update ThinkInstallF.sh

--- ThinkInstallF.sh
+++ ThinkInstallF.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+##########################################################################################################################
+# Script: ThinkInstallF
+# Autor: ThinkRoot
+# Versiune: 1.0
+
+# Descriere: 
+#   Acest script automatizează procesul de instalare a unui set de programe și dependințe pe un sistem Fedora.
+#   Este util pentru instalarea unui set comun de programe și personalizări pe un sistem Fedora.
+
+# Pași de utilizare:
+# 1. După ce ai descărcat acest script, șterge programele de care nu ai nevoie și adaugă programele necesare pentru tine.
+# 2. Deschide terminalul și navighează în directorul în care ai salvat scriptul.
+# 3. Acordă permisiuni de executare pentru script folosind comanda: chmod +x ThinkInstallF.sh.
+# 4. Rulează scriptul cu privilegii de superutilizator (sudo) folosind comanda: sudo ./ThinkInstallF.sh.
+# 5. Scriptul va începe instalarea programelor și a personalizărilor specificate.
+##########################################################################################################################
+
 # Actualizare sistem
 sudo dnf upgrade --refresh
 
@@ -15,24 +32,24 @@ sudo dnf install rpmfusion-free-release-tainted
 sudo dnf install libdvdcss
 sudo dnf install vlc-plugins-freeworld
 
-# Hardware acceleration
+# Instalare drivere pentru Intel
 sudo dnf install intel-media-driver
 sudo dnf install libva-intel-driver
 
-# Firmware
+# Instalare Firmware
 sudo dnf install rpmfusion-nonfree-release-tainted
 sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware"
 
 # Actualizare sistem
 sudo dnf upgrade --refresh
 
-# Instalare programe
+# Instalare programe rpm
 sudo dnf install gnome-tweak gimp gimp-data-extras gimp-elsamuko gimp-paint-studio gimp-resynthesizer gimpfx-foundry gmic-gimp gimp-jxl-plugin libreoffice libreoffice-calc libreoffice-core libreoffice-data libreoffice-emailmerge libreoffice-gtk3 libreoffice-gtk4 libreoffice-impress libreoffice-pdfimport libreoffice-writer kdenlive obs-studio obs-studio-plugin-vkcapture obs-studio-plugin-vlc-video obs-studio-plugin-webkitgtk obs-studio-plugin-x264 vlc vlc-extras telegram-desktop discord filezilla polari qbittorrent gparted apostrophe file-roller pdfarranger rpi-imager git neofetch htop yt-dlp pandoc
 
-# Instalare flatpak
+# Instalare programe flatpak
 flatpak install flathub com.github.gijsgoudzwaard.image-optimizer org.nickvision.tubeconverter com.spotify.Client io.gitlab.adhami3310.Converter eu.betterbird.Betterbird one.ablaze.floorp com.google.Chrome io.gitlab.librewolf-community com.microsoft.Edge com.github.eneshecan.WhatsAppForLinux org.gnome.World.PikaBackup org.gnome.World.Iotas com.bitwarden.desktop io.github.idevecore.CurrencyConverter com.mattjakeman.ExtensionManager com.github.tchx84.Flatseal net.cozic.joplin_desktop com.microsoft.Edge org.onlyoffice.desktopeditors com.vscodium.codium io.github.shiftey.Desktop io.github.zhrexl.thisweekinmylife
 
-# Instalare extensii
+# Instalare extensii pentru GNOME
 sudo dnf install gnome-shell-extension-user-theme gnome-shell-extension-appindicator gnome-shell-extension-dash-to-dock gnome-shell-extension-blur-my-shell
 
 # Instalare teme
